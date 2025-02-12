a7452824debd8746c1b2217b362d1f90d5279df1
sametkapar <130076921+sametkapar@users.noreply.github.com>
2025-02-12 16:04:00 +0300

    Challenge 2 Done

---
+++ Unity Learning - Projects/Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldown = 1.0f;
+    private float lastspawnTime=0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > lastspawnTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastspawnTime = Time.time + cooldown;   
+        }
+    }
+}
